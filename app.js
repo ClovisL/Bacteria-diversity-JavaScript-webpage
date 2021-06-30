0f7741136aefa36e17ca2601b368b3cebb656fd6
ClovisL <clacerd@outlook.com>
2021-06-29 21:01:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,21 +12,28 @@ d3.json("data/samples.json").then((data) => {
 
     //  Create the Traces
   var trace1 = {
-    x: data.samples.otu_ids,
-    y: data.samples.sample_values,
+    x: data.map(row => row.sample.otu_ids),
+    y: data.map(row => row.sample.sample_values),
+    text: data.map(row => row.samples.otu_labels),
     type: "bar",
     orientation: "h",
     name: data.samples.otu_labels,
   };
 
   // Create the data array for the plot
-  var data = [trace1];
+  var chartData = [trace1];
 
   // Define the plot layout
   var layout = {
     title: "Top 10 Bacteria Cultures Found",
+    margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100
+    }
   };
 
   // Plot the chart to a div tag with id "plot"
-  Plotly.newPlot("plot", data, layout);
+  Plotly.newPlot("plot", chartData, layout);
 });
