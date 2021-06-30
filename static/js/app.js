e93bce486be1577d5d4ac023d2a36a2d2538c569
ClovisL <clacerd@outlook.com>
2021-06-29 20:42:55 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // Use d3.json() to fetch data from JSON file
-d3.json("data/data.json").then((data) => {
+d3.json("./samples.json").then((data) => {
     let selector = d3.select("#selDataset");
     let sampleNames = data.map(ele => ele.names);
             
