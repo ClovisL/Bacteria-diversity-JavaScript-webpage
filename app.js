94b45a98cc88d3d90f46c610741f3b8cce183ee0
ClovisL <clacerd@outlook.com>
2021-06-29 20:54:46 -0400

    update

--- app.js
+++ app.js
@@ -9,4 +9,24 @@ d3.json("data/samples.json").then((data) => {
             .text(sample)
             .property("value", sample);
     });
+
+    //  Create the Traces
+  var trace1 = {
+    x: data.samples.otu_ids,
+    y: data.samples.sample_values,
+    type: "bar",
+    orientation: "h",
+    name: data.samples.otu_labels,
+  };
+
+  // Create the data array for the plot
+  var data = [trace1];
+
+  // Define the plot layout
+  var layout = {
+    title: "Top 10 Bacteria Cultures Found",
+  };
+
+  // Plot the chart to a div tag with id "plot"
+  Plotly.newPlot("plot", data, layout);
 });
